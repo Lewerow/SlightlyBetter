dd7d5a355d5810dfd1678c251b77b182d56d56fc
Tomasz Lewowski <tomasz.lewowski@wp.eu>
2016-07-09 23:43:04 +0200

    Code changes for 09.07.2016 post - removed old styles and replaced them with Bootstrap. Also templated the Hamlet template to take data from Haskell

--- Handler/Projects.hs
+++ Handler/Projects.hs
@@ -1,9 +1,12 @@
 module Handler.Projects where
 import Import
 
-import Yesod.Form.Bootstrap3()
+import Yesod.Bootstrap()
+
 
 getProjectsR :: Handler Html
 getProjectsR = defaultLayout $ do
-	setTitle "Projects"
-	$(widgetFile "projects")
+  setTitle "Projects"
+  $(widgetFile "projects")
+    where
+      projects = ["Project 1", "Project 2", "My extra project", "Project 10"]::[Text]
